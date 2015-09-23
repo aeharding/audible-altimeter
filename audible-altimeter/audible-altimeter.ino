86f49fc80972bdf43a24f47729f1499ce827a83a
Alexander Harding <alexander.harding@software.dell.com>
2015-09-23 13:52:35 -0500

    changes for the second (real) test of the second prototype!

--- audible-altimeter/audible-altimeter.ino
+++ audible-altimeter/audible-altimeter.ino
@@ -24,11 +24,11 @@ void BMPSetup() {
     // see the comments at the top of this sketch for the proper connections.
 
     Serial.println("BMP180 init fail (disconnected?)\n\n");
-    while(1); // Pause forever.
+    while (1); // Pause forever.
   }
 
   // Get the baseline pressure:
-  
+
   basePressure = getPressure();
   previousPressure = basePressure;
 }
@@ -46,20 +46,29 @@ void BMPSetup() {
  ***************************************************/
 void setup()
 {
-  Serial.begin(9600);
-  Serial.println("Initialising...");
+  //  Serial.begin(9600);
+  //  Serial.println("Initialising...");
 
-  pinMode(LED_PIN, OUTPUT);
+  //  pinMode(LED_PIN, OUTPUT);
 
-  delay(100); //Allow for serial print to complete.
+  //  delay(100); //Allow for serial print to complete.
 
   BMPSetup();
   inPlaneTriggered = false;
 
   sleepPeriod = SLEEP_8S;
 
-  Serial.println("Initialisation complete.");
-  delay(100); //Allow for serial print to complete.
+  //  Serial.println("Initialisation complete.");
+
+  tone(3, 2048);
+  delay(50);
+  noTone(3);
+  delay(50);
+  tone(3, 2048);
+  delay(50);
+  noTone(3);
+
+  //  delay(100); //Allow for serial print to complete.
 }
 
 /***************************************************
@@ -74,28 +83,26 @@ void setup()
  ***************************************************/
 void loop()
 {
+  int i;
   double currentPressure, currentAltitude;
-  
-  /* Toggle the LED */
-  digitalWrite(LED_PIN, HIGH);
 
   currentPressure = getPressure();
-  
-  Serial.print("Current pressure:");
-  Serial.println(currentPressure);
-  
+
+  //  Serial.print("Current pressure:");
+  //  Serial.println(currentPressure);
+
   currentAltitude = pressure.altitude(currentPressure, basePressure);
-  
-  Serial.print("Current altitude:");
-  Serial.println(currentAltitude);
+
+  //  Serial.print("Current altitude:");
+  //  Serial.println(currentAltitude);
 
   if (currentAltitude < 5 && currentAltitude > -5 && !inPlaneTriggered) {
-    Serial.println("On ground - adjusting for altitude");
+    //    Serial.println("On ground - adjusting for altitude");
     sleepPeriod = SLEEP_8S;
     // consider this noise, set as base
     basePressure = currentPressure;
   } else {
-    Serial.println("In plane");
+    //    Serial.println("In plane");
     inPlaneTriggered = true;
     // we're in the air
     sleepPeriod = SLEEP_1S;
@@ -103,26 +110,44 @@ void loop()
     // human falls at 53 m/s
     // open canopy, full flight 5 m/s
     // refresh rate is 1 s on arduino
-    Serial.print("DIFF:");
-    Serial.println(previousAltitude - currentAltitude);
-    if (previousAltitude - currentAltitude > 5 && // m/s
-           currentAltitude < 1524) { // 3000 ft
-      Serial.println("TRIGGER!");
-      // beep and wait for slowing down
-//      tone(2, 2048);
-//      LowPower.powerSave(SLEEP_1S, ADC_OFF, BOD_ON, TIMER2_ON);
-//      noTone(2);
+    //    Serial.print("DIFF:");
+    //    Serial.println(previousAltitude - currentAltitude);
+    while (previousAltitude - currentAltitude > 30 && // m/s
+           currentAltitude < 1371) { // 4500 ft
+      currentPressure = getPressure();
+      currentAltitude = pressure.altitude(currentPressure, basePressure);
+      //      Serial.println("TRIGGER!");
+      beep1s();
+      previousPressure = currentPressure;
+      previousAltitude = currentAltitude;
     }
   }
-  
+
   previousPressure = currentPressure;
   previousAltitude = currentAltitude;
 
-  digitalWrite(LED_PIN, LOW);
+  //  delay(100); // for console
 
-  delay(100); // for console
-  
   LowPower.powerDown(sleepPeriod, ADC_OFF, BOD_ON);
+}
+
+
+void beep1s() {
+  // 142.8
+  int i;
+  for (i = 0; i < 10; i++) {
+    tone(3, 1900);
+    delay(30);
+    noTone(3);
+    delay(70);
+  }
+  
+//  int i;
+//  for (i = 1800; i < 2500; i += 20) {
+//    tone(3, i);
+//    delay(5);
+//    noTone(3);
+//  }
 }
 
 
@@ -130,10 +155,10 @@ void loop()
 double getPressure()
 {
   char status;
-  double T,P,p0,a;
+  double T, P, p0, a;
 
   // You must first get a temperature measurement to perform a pressure reading.
-  
+
   // Start a temperature measurement:
   // If request is successful, the number of ms to wait is returned.
   // If request is unsuccessful, 0 is returned.
@@ -171,10 +196,10 @@ double getPressure()
         // (If temperature is stable, you can do one temperature measurement for a number of pressure measurements.)
         // Function returns 1 if successful, 0 if failure.
 
-        status = pressure.getPressure(P,T);
+        status = pressure.getPressure(P, T);
         if (status != 0)
         {
-          return(P);
+          return (P);
         }
         else Serial.println("error retrieving pressure measurement\n");
       }
